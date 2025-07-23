06bf95477309b804ea7080da3e2757945c2c9a16
Yeawrongperson <50104300+yeawrongperson@users.noreply.github.com>
2025-07-23 12:06:31 -0400

    Fetch events at launch

---
+++ LCEMonitorApp/Model/DispatchEvent.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct DispatchEvent: Identifiable, Codable {
+    let id: UUID
+    let title: String
+    let detail: String
+
+    init(id: UUID = UUID(), title: String, detail: String) {
+        self.id = id
+        self.title = title
+        self.detail = detail
+    }
+}
